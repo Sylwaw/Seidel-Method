d1cedbebf38e7f4f584adf4ee2f03664336c5b28
Sylwia Wawrzuta <sylwia.wawrzuta@gmail.com>
2019-01-07 23:50:56 +0100

    poprawki1

--- MN/Matrix.h
+++ MN/Matrix.h
@@ -65,55 +65,55 @@ public:
 			}
 			file.close(); // konczenie polaczenia z plikiem
 			std::cout << "\nROZMIAR: " << size;
-			print(size, tabMatrixA, tabVectorB,"MACIERZ A", "WEKTOR B"); // wypisywanie macierzy i wektora do konsoli
+			print(size, tabMatrixA, tabVectorB, "MACIERZ A", "WEKTOR B"); // wypisywanie macierzy i wektora do konsoli, coœ jest Ÿle bo na przek¹tnej powinny byæ same zera
 		}
-
-
+		double** matrixAlfa = createTabMatrixAlfa(tabMatrixAlfa, size, tabMatrixA);
+		double* vectorBeta = createTabVectorBeta(tabVectorBeta, size, tabVectorB, tabMatrixA);
+		tabX = createTabx(tabX, size, tabVectorB);
+		print(size, matrixAlfa, tabVectorB, "ALFA", "BETA");
+		double* seidel = seidelMethod(tabMatrixA,matrixAlfa, vectorBeta, tabX, size);
 	}
 
-	double* seidelMethod(double** tabMatrixAlfa, double* tabVectorBeta, double* tabX, int size) // to jest to co robilas???
+	double* seidelMethod(double** tabMatrixA, double** tabMatrixAlfa, double* tabVectorBeta, double* tabX, int size) // to jest to co robilas???
 	{
-		int i, j, iterationNumber = 0;
-		double normI;
-		if (i == j && tabMatrixAlfa[i][j] == 0)
-		{
-			std::cout << "Nie mozna wykonac obliczen - nie mo¿na dzieliæ przez zero!!!!!";
-		    exit(1);
-		}
-		else
+		int i = 0, j = 0, iterationNumber = 0;
+		double normI = 0;
+		double sum1 = 0, sum2 = 0;
+		bool a = divideByZero(tabMatrixA, size);
+
+		if (a == false)
 		{
 			do {
-				int iterationNumber = 0;
-				double sum1 = 0, sum2 = 0;
-				if (iterationNumber = 0)
+
+				if (iterationNumber == 0) //powinno w to wejœæ gdzie jest to add to watch -.-
 				{
-					for(int i = 0; i < size; i++)
-					tabX[i] = tabVectorBeta[i];
+					for (int i = 0; i < size; i++)
+						tabX[i] = tabVectorBeta[i];
 					iterationNumber++;
 				}
 				else
 				{
 					for (int it = 0; it < size; it++)
 					{
-						if (it = 0)
+						if (it == 0)//albo chyba jednak jest ok, juz sie w tym pogubilam troche, mysle ¿e jest ok czyli powinno dzialac ja:D?
 						{
 							for (int j = 1; j < size; j++)
 							{
-								sum1 += tabMatrixAlfa[i][j] * tabX[j];
+								sum1 += tabMatrixAlfa[i][j] * tabX[j];//a tu by³o ok?
 							}
 							tabX[it] = sum1 + tabVectorBeta[it];
 							iterationNumber++;
 						}
 						else
 						{
-							for (int i = 1; i <= size; j++)
+							for (int i = 1; i <= size; i++)
 							{
-								for (int j = 0; j <= size - 1; j++)
+								for (int j = 0; j <= size - 1; j++)//a mo¿e wypiszmy najpierw alfe i bete mo¿e przy tworzeniu jest b³¹d?
 								{
-									sum1 += tabMatrixAlfa[i][j] * tabX[j];
+									sum1 += tabMatrixAlfa[i][j] * tabX[j];  // czekaj stop bo widze b³¹d we wzorze, zobacz sobie w tym czasie na to widzisz tu co? a co tu jest nie tak? tak chocby nie umiol komorki odczytac
 								}
 							}
-							for (int i = 1; i <= size; j++)
+							for (int i = 1; i <= size; i++)
 							{
 								for (int j = 2; j <= size; j++)
 								{
@@ -124,19 +124,19 @@ public:
 							iterationNumber++;
 						}
 					}
-					
+
 				}
-				
-			} while (normI <= epsilon || iterationNumber >= MLI);
+			} while (normI <= epsilon && iterationNumber >= MLI);
 		}
-		return tabX;
+
+		return tabX;//to sie mog³o nigdy nie koñczyæ chyba kuzwa no moze tak byc :D
 		//dodaæ sprawdzanie normy
 	}
 
 	void generateRaportToFile(std::string fileName, int ** tabMatrixA, int * tabVectorB, int ** tabMatrixAlfa, int * tabVectorBeta) { // funkcja generujaca raport do pliku
-		
+
 	}
-	
+
 	void setMLI() { // wczytywanie maxymalnej liczby iteracji
 		std::cout << "Podaj maxymalna liczbe iteracji: ";
 		std::cin >> MLI;
@@ -148,9 +148,11 @@ public:
 
 private:
 	std::list<double> listTMP; // zmienna uzyta dla okreslenia wielkosci macierzy
-	double ** tabMatrixA = 0, tabMatrixAlfa = 0; // sylwia te zmienne to juz przygotowane do tworzenia alfa i beta
-	double * tabVectorB = 0, tabVectorBeta = 0;
-	double* tabX = 0; 
+	double ** tabMatrixA = 0;
+	double ** tabMatrixAlfa = 0;
+	double * tabVectorB = 0;
+	double * tabVectorBeta = 0;
+	double* tabX = 0;
 	double epsilon = 0.0001, x = 0;
 	int MLI = 0;
 
@@ -165,7 +167,7 @@ private:
 		for (int i = 0; i < size; i++)
 		{
 			tabMatrix[i] = new double[size];
-		}		
+		}
 		return tabMatrix; // a nie szlo z tych skorzystac? to jest to samo
 	}
 
@@ -179,12 +181,12 @@ private:
 	}
 
 	void print(int size, double ** tabMatrix, double * tabVector, std::string matrixA, std::string vectorB) { // wypisywanie wartosci do konsoli
-		std::cout << "\n" <<matrixA << std::endl;
+		std::cout << "\n" << matrixA << std::endl;
 		for (int i = 0; i < size; i++)
 		{
 			for (int j = 0; j < size; j++)
 			{
-				std::cout << tabMatrix[i][j] << " ";
+				std::cout << tabMatrix[i][j] << "\t\t";
 			}
 			std::cout << "\n";
 		}
@@ -196,11 +198,20 @@ private:
 		}
 	}
 
+	void printX(int size, double *tabX)
+	{
+		std::cout << "tabX" << std::endl;
+		for (int i = 0; i < size; i++)
+		{
+			std::cout << tabX[i] << ", ";
+		}
+	}
+
 	double* createTabVector(double * tabVector, int size) { // funkcja alokujaca pamiec dla nowego wektora
 		return tabVector = new double[size];
 	}
 
-	double* createTabx(double * tabX, int size) { // funkcja alokujaca pamiec dla nowego wektora
+	double* createTabx(double * tabX, int size, double* tabVectorB) { // funkcja alokujaca pamiec dla nowego wektora
 		tabX = new double[size];
 		for (int i = 0; i < size; i++)
 		{
@@ -229,7 +240,7 @@ private:
 		{
 			for (int k = 0; k < size; k++)
 			{
-				if (j = k)
+				if (j == k)
 				{
 					tabMatrixAlfa[j][k] = 0.0;
 				}
@@ -237,11 +248,29 @@ private:
 				{
 					tabMatrixAlfa[j][k] = -(tabMatrixA[j][k] / tabMatrixA[j][j]);
 				}
-				
+
 			}
 		}
 
 		return tabMatrixAlfa;
+	}
+
+	bool divideByZero(double **matrixA, int size)
+	{
+		bool a = false;
+		for (int i = 0; i < size; i++)
+		{
+			for (int j = 0; j < size; j++)
+			{
+				if (i == j && matrixA[i][j] == 0)
+				{
+					std::cout << "Obliczenia przerwane, nie mo¿na dzieliæ przez 0" << std::endl;
+					a = true;
+					exit(1);
+				}
+			}
+		}
+		return a;
 	}
 
 
